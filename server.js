6cea7b99ac48c9f431b408e448694f2813e13a46
afzalahmmed <ahmmedafzal4@gmail.com>
2022-07-04 18:00:27 +0600

    complete upto #4

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer(
     (req, res) => {
-        console.log('request was made: ');
+        console.log(req.url);
+        res.setHeader('Content-Type', 'text/html');
+
+        let path = './views/';
+        switch(req.url){
+            case '/':
+                path += 'index.html';
+                break;
+            case '/about':
+                path += 'about.html';
+                break;
+            default:
+                path += '404.html';
+        }
+
+        fs.readFile(path, (err, data) => {
+            if(err){
+                console.log(err);
+                res.end();
+            }
+            res.end(data);
+        });
     }
 )
 
