f1f167c513955234d017fcb5d7515c8eb61c924e
ahmed-afzal1 <afzalbhairab4@gmail.com>
2022-07-07 04:01:46 +0600

    half of #7

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs');
+// app.set('views', './views');
+
 app.get('/', function (req, res) {
-    res.sendFile('/views/index.html',{root:__dirname})
+    res.render('index',{title:'Home'});
 })
 
 app.get('/about', function (req, res) {
-    res.sendFile('/views/about.html',{root:__dirname})
+    res.render('about',{title:'About'});
 })
+
+app.get('/blogs/create', function (req, res) {
+    res.render('create',{title:'Blog create'});
+});
 
 app.get('/about-us', function (req, res) {
     res.redirect('/about');
 })
 
 app.use((req,res)=>{
-    res.status(404).sendFile('/views/404.html',{root:__dirname})
+    res.status(404).render('404',{title:'404'});
 })
 
-app.listen(3000)
+app.listen(3000);
